78a8fe48dc43318261d69d673df71bf217cc2c64
JAAR91 <jaarriagaramos@gmail.com>
2021-04-07 16:23:11 -0600

    project sql

--- database.sql
+++ database.sql
@@ -107,8 +107,104 @@ SELECT name FROM bbc
 Bangladesh
 India
 Pakistan
+ /*SUM and COUNT Quiz*/
+SELECT SUM(population) FROM bbc WHERE region = 'Europe'
+
+SELECT COUNT(name) FROM bbc WHERE population < 150000
+
+AVG(), COUNT(), MAX(), MIN(), SUM()
+
+No result due to invalid use of the WHERE function
+
+SELECT AVG(population) FROM bbc WHERE name IN ('Poland', 'Germany', 'Denmark')
+
+SELECT region, SUM(population)/SUM(area) AS density FROM bbc GROUP BY region
+
+SELECT name, population/area AS density FROM bbc WHERE population = (SELECT MAX(population) FROM bbc)
+
+Americas	732240
+Middle East	13403102
+South America	17740392
+South Asia	9437710
  /**/
+game  JOIN goal ON (id=matchid)
  
+matchid, teamid, player, gtime, id, teamname, coach
+
+SELECT player, teamid, COUNT(*)
+  FROM game JOIN goal ON matchid = id
+ WHERE (team1 = "GRE" OR team2 = "GRE")
+   AND teamid != 'GRE'
+ GROUP BY player, teamid
+
+DEN	9 June 2012
+GER	9 June 2012
+
+  SELECT DISTINCT player, teamid 
+   FROM game JOIN goal ON matchid = id 
+  WHERE stadium = 'National Stadium, Warsaw' 
+ AND (team1 = 'POL' OR team2 = 'POL')
+   AND teamid != 'POL'
+
+   SELECT DISTINCT player, teamid, gtime
+  FROM game JOIN goal ON matchid = id
+ WHERE stadium = 'Stadion Miejski (Wroclaw)'
+   AND (( teamid = team2 AND team1 != 'ITA') OR ( teamid = team1 AND team2 != 'ITA'))
+
+
+Netherlands	2
+Poland	2
+Republic of Ireland	1
+Ukraine	2
+ /*JOIN Quiz 2*/
+SELECT name
+  FROM actor INNER JOIN movie ON actor.id = director
+ WHERE gross < budget
+
+ SELECT *
+  FROM actor JOIN casting ON actor.id = actorid
+  JOIN movie ON movie.id = movieid
+
+  SELECT name, COUNT(movieid)
+  FROM casting JOIN actor ON actorid=actor.id
+ WHERE name LIKE 'John %'
+ GROUP BY name ORDER BY 2 DESC
+
+ "Crocodile" Dundee
+Crocodile Dundee in Los Angeles
+Flipper
+Lightning Jack
+
+SELECT name
+  FROM movie JOIN casting ON movie.id = movieid
+  JOIN actor ON actor.id = actorid
+WHERE ord = 1 AND director = 351
+
+link the director column in movies with the primary key in actor
+connect the primary keys of movie and actor via the casting table
+
+A Bronx Tale	1993
+Bang the Drum Slowly	1973
+Limitless	2011
+ /*Using Null Quiz*/
+ SELECT teacher.name, dept.name FROM teacher LEFT OUTER JOIN dept ON (teacher.dept = dept.id)
+
+ SELECT dept.name FROM teacher JOIN dept ON (dept.id = teacher.dept) WHERE teacher.name = 'Cutflower'
+
+ SELECT dept.name, COUNT(teacher.name) FROM teacher RIGHT JOIN dept ON dept.id = teacher.dept GROUP BY dept.name
+
+ display 0 in result column for all teachers without department
+
+ 'four' for Throd
+
+ Shrivell	Computing
+Throd	Computing
+Splint	Computing
+Spiregrain	Other
+Cutflower	Other
+Deadyawn	Other
+ /**/
+ /**/
  /**/
  /**/
  
